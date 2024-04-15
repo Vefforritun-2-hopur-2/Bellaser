1c8722a87bc2673288e2b277eb65cd9684c0e4be
Brakku <88351016+Brakku@users.noreply.github.com>
2024-04-15 09:39:05 +0000

    incompleate

---
+++ bellester/src/app/tstest.tsx
@@ -0,0 +1,12 @@
+//setja in functions hér sem er hægt að importa
+
+interface proper{
+    link:string
+}
+
+export default function Linkbutton(proper)
+{
+
+    <a href={proper}>test link med {prop}</a>
+
+}
---
+++ bellester/src/components.tsx
@@ -0,0 +1,7 @@
+//setja in functions hér sem er hægt að importa
+
+export default function Linkbutton(link: string | undefined){
+
+    <a href={link}>test link</a>
+
+}
