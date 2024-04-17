5b7fcb9bf15b0f03737ccc55511542ad8a5b01f9
Brakku <88351016+Brakku@users.noreply.github.com>
2024-04-17 15:08:35 +0000

    lagaði hluti

--- bellester/src/app/page.tsx
+++ bellester/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Linkbutton} from '../components'
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       {/* Meira innihald hér */}
+      < Linkbutton link="about" text='About us'/>
     </div>
   );
 };
--- bellester/src/components.tsx
+++ bellester/src/components.tsx
@@ -1,9 +1,9 @@
 //setja in functions hér sem er hægt að importa
 import React from 'react';
-export function Linkbutton(props:{link:string})
+export function Linkbutton(props:{link:string,text:string})
 {
 
-    return <a href={props.link}></a>
+    return <a href={props.link}>{props.text}</a>
 
 }
 
