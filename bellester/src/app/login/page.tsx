538d991f93e5529475e59c3807cfe577e55670a8
Kristján Viktor Steinarsson <100709790+kristjansteinarsson@users.noreply.github.com>
2024-04-29 11:01:29 +0000

    bæta við hlutir í login page

--- bellester/src/app/login/page.tsx
+++ bellester/src/app/login/page.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+// import Image from 'next/image';
+// import phone from './image/phone.svg'
+// import google from './image/google.svg'
 
 const Login = () => {
   return (
     <div>
         
-      <h1>Login Us</h1>
-      {/* Meira innihald hér */}
+      
+      <div className='signupLogin'>
+        <h1>Login</h1>
+        <div className='loginTemplate'></div>
+        {/* <h2>Login with Google <Image src={google} alt='googleLogo' height={500}></Image></h2> */}
+        {/* <h2>Login with Phone <Image src={phone} alt='phoneIcon' height={500}></Image></h2> */}
+
+        <h3>Don't have an account?</h3>
+        <button>Sign Up</button>
+      </div>
+
     </div>
   );
 };
