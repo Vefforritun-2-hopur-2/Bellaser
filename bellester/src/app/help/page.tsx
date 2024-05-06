7b34ca327413099f6a0d8904f4486e6cd18c621e
Kristján Viktor Steinarsson <100709790+kristjansteinarsson@users.noreply.github.com>
2024-05-06 11:45:46 +0000

    Aweomse stuff

--- bellester/src/app/help/page.tsx
+++ bellester/src/app/help/page.tsx
@@ -2,14 +2,36 @@ import React from 'react';
 
 const Help = () => {
   return (
-    <div className='hjalpSida'>
-      <h1>Hjálp og Aðstoð</h1>
-      <div className='hjalpBox'>
-        <h1>
-          <a href="https://youtu.be/9gCfWpOVHu4" target='_blank'>Tutorial á síðunni</a>
-        </h1>
+    <>
+      <div className='umOkkur'>
+        <h1>Hjálp og Aðstoð</h1>
+        <div className='umOkkurTexti'>
+          <p>
+            Hvað er Bellaser?
+
+            Bellaser er öflugt tæki sem býður upp á nýjungar í heiminum hunda- og kattaeigenda. Þetta er ekki bara app, heldur vist fyrir félagslegan hring kennilegur fólks sem hlýtur að þjóna þínum áhugum og þörfum fyrir gæludýraþjónustu.
+
+Hugmyndin er einföld: samanfletta fólk sem hefur til samfélagslegs áhuga og þörfir til umsýslu, pössunar eða einfaldlega gönguferða með þeim sem hafa gæludýr í umönnun. Með Bellaser getur þú skráð þig sem eigandi eða sem þjónustufulltrúi, miðlað til sín milli og skapað nýjar tengingar.
+
+Það sem skilur Bellaser frá öðrum tækjum er samfelld samskipti milli notenda. Þú getur samskipt við þjónustufulltrúa, skoðað umsagnir og raðað þjónustu eftir þínum þörfum. Það er ekki bara um að fá úrræði fyrir þjónustu, heldur um að byggja samfélag þar sem gæludýraæsku og umönnun er í forgrunni.
+
+Með Bellaser getur þú:
+
+Skráð og stjórnað þínum gæludýraþjónustu þarfir beint frá snjallsímanum þínum.
+Leitað að áreiðanlegum þjónustufulltrúum sem bjóða upp á þjónustu sem passar þínum skilyrðum og þörfum.
+Skoðað umsagnir og einkunnir frá öðrum notendum til að tryggja gæði og traust.
+Búið til samfélag innan appins með deilum, radíóvélar og viðburði.
+Með Bellaser er hundagöngan, kattapössunin og umönnunin við gæludýrið þitt einfaldari og skemmtilegri en nokkurn tíma áður. Byggðu nýja tengsl, styrk þína samkennd og gakktu handan þess sem þú átt að veita og þig nauðsynlegur. Bellaser - fyrir gæludýraæskuna í þér.
+          </p>
+        <h2>
+          Myndbönd:
+        </h2>
+        <a href="https://youtu.be/9gCfWpOVHu4" target='_blank'>- Tutorial á síðunni</a>
       </div>
-    </div>
+      </div>
+       
+      
+    </>
   );
 };
 
