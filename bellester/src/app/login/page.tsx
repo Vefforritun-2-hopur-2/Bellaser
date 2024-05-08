be346c8f1880520f880ca558986c330df9a0fb03
Kristjan <steinarssonk@gmail.com>
2024-05-08 15:57:39 +0000

    Smá login breytingu

--- bellester/src/app/login/page.tsx
+++ bellester/src/app/login/page.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import phone from '../../image/phone.svg';
-import google from '../../image/google.svg';
 const Login = () => {
   return (
     <div>
       <div className='signupLogin'>
-        <h1>Login</h1>
-        <div className='loginTemplate'></div>
-
-        <div className='loginWithIcon'>
-          <h2>Login with Google</h2>
-          <Image src={google} alt='googleLogo' height={500} width={500}></Image>
-        </div>
-
-        <div className='loginWithIcon'>
-          <h2>Login with Phone</h2>
-          <Image src={phone} alt='phoneIcon' height={500} width={500}></Image>
-        </div>  
+        
+        <button className='loginWithIcon'>Login</button>
 
         <h3>Don't have an account?</h3>
         <a href="/signup" className="block text-white px-4 py-2">Sign Up</a>
