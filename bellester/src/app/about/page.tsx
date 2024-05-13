39727a4bad76ea24a7dbb9cc41923c95f7cb01fd
Kristján Viktor Steinarsson <100709790+kristjansteinarsson@users.noreply.github.com>
2024-05-13 09:39:21 +0000

    Guðni breytti About texta

--- bellester/src/app/about/page.tsx
+++ bellester/src/app/about/page.tsx
@@ -14,10 +14,10 @@ export default function About() {
       <div className='umOkkurTexti'>
 
       <p>
-    Við bjóðum upp á nýja og áreiðanlega leið til að hjálpa gæludýraeigendum að finna umönnun fyrir fjölskylduvinina á fjórufætlingaformi. Með Bellaser appinu getur þú snúið þér til þeirra sem bjóða þjónustu í hundapössun og göngutúra, og það allt á einfaldan og áreiðanlegan hátt.
+    Við bjóðum upp á nýja og áreiðanlega leið til að hjálpa gæludýraeigendum að finna umönnun fyrir fjölskylduvinina. Með Bellaser appinu getur þú snúið þér til þeirra sem bjóða þjónustu í hundapössun, göngutúra og fleira, og það allt á einfaldan og áreiðanlegan hátt.
     </p>
     <Image src={dog2} alt='labbaHund' height={500}></Image>
-    Fyrir okkur er hver gæludýr sérstakt, og okkar markmið er að tryggja hamingju og heilbrigði þeirra með áreiðanlega umönnun. Með Bellaser appinu getur þú verið viss um að gæludýrið þitt fær bestu mögulegu umönnunina, hvort sem það er í formi göngutúra eða heimilisfóstrar.
+    Fyrir okkur eru öll gæludýr sérstök á sinn og öll þurfa mismunandi umönnun, og okkar markmið er að tryggja hamingju og heilsu þeirra með góðri og skemmtilegri umönnun. Með Bellaser appinu getur þú verið viss um að gæludýrið þitt fær bestu mögulegu pössunina, hvort sem það er í formi göngutúra, pössun og fleira.
     <div className='umOkkurMyndir'>
     <Image src={dog3} alt='labbaHund' height={500}></Image>
     <Image src={dog1} alt='labbaHund' height={500}></Image>
