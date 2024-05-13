558a6ea1615ea924917131b6cfb93e0e6cf782da
Kristján Viktor Steinarsson <100709790+kristjansteinarsson@users.noreply.github.com>
2024-05-13 11:44:51 +0000

    Bætti við myndir frá annari branch og breytti about me page

--- bellester/src/app/about/page.tsx
+++ bellester/src/app/about/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+import dog1 from '../../image/dogPhoto-1.jpg'
+import dog2 from '../../image/dogPhoto-2.jpg'
+import dog3 from '../../image/dogPhoto-3.jpg'
+
 
 export default function About() {
   return (
-    <div>
-      <h1>About Us</h1>
-      {/* Meira innihald hér */}
+    <div className='umOkkur'>
+      <h1>
+      Velkomin á Bellaser – um gæludýrirð þitt!
+      </h1>
+      <div className='umOkkurTexti'>
+
+      <p>
+    Við bjóðum upp á nýja og áreiðanlega leið til að hjálpa gæludýraeigendum að finna umönnun fyrir fjölskylduvinina. Með Bellaser appinu getur þú snúið þér til þeirra sem bjóða þjónustu í hundapössun, göngutúra og fleira, og það allt á einfaldan og áreiðanlegan hátt.
+    </p>
+    <Image src={dog2} alt='labbaHund' height={500}></Image>
+    Fyrir okkur eru öll gæludýr sérstök á sinn og öll þurfa mismunandi umönnun, og okkar markmið er að tryggja hamingju og heilsu þeirra með góðri og skemmtilegri umönnun. Með Bellaser appinu getur þú verið viss um að gæludýrið þitt fær bestu mögulegu pössunina, hvort sem það er í formi göngutúra, pössun og fleira.
+    <div className='umOkkurMyndir'>
+    <Image src={dog3} alt='labbaHund' height={500}></Image>
+    <Image src={dog1} alt='labbaHund' height={500}></Image>
+    </div>
+    <h2>Takk fyrir að vera hluti af Bellaser samfélaginu!</h2>
+    </div>
     </div>
   );
 };
--- bellester/src/app/help/page.tsx
+++ bellester/src/app/help/page.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import Image from 'next/image';
-import dog1 from '../../image/dogPhoto-1.jpg'
-import dog2 from '../../image/dogPhoto-2.jpg'
-import dog3 from '../../image/dogPhoto-3.jpg'
 
-
-export default function About() {
+const Help = () => {
   return (
-    <div className='umOkkur'>
-      <h1>
-      Velkomin á Bellaser – um gæludýrirð þitt!
-      </h1>
-      <div className='umOkkurTexti'>
+    <>
+      <div className='umOkkur'>
+        <h1>Hjálp og Aðstoð</h1>
+        <div className='umOkkurTexti'>
+          <p>
+            Hvað er Bellaser?
 
-      <p>
-    Við bjóðum upp á nýja og áreiðanlega leið til að hjálpa gæludýraeigendum að finna umönnun fyrir fjölskylduvinina. Með Bellaser appinu getur þú snúið þér til þeirra sem bjóða þjónustu í hundapössun, göngutúra og fleira, og það allt á einfaldan og áreiðanlegan hátt.
-    </p>
-    <Image src={dog2} alt='labbaHund' height={500}></Image>
-    Fyrir okkur eru öll gæludýr sérstök á sinn og öll þurfa mismunandi umönnun, og okkar markmið er að tryggja hamingju og heilsu þeirra með góðri og skemmtilegri umönnun. Með Bellaser appinu getur þú verið viss um að gæludýrið þitt fær bestu mögulegu pössunina, hvort sem það er í formi göngutúra, pössun og fleira.
-    <div className='umOkkurMyndir'>
-    <Image src={dog3} alt='labbaHund' height={500}></Image>
-    <Image src={dog1} alt='labbaHund' height={500}></Image>
-    </div>
-    <h2>Takk fyrir að vera hluti af Bellaser samfélaginu!</h2>
-    </div>
-    </div>
+            Bellaser er öflugt tæki sem býður upp á nýjungar í heiminum hunda- og kattaeigenda. Þetta er ekki bara app, heldur vist fyrir félagslegan hring kennilegur fólks sem hlýtur að þjóna þínum áhugum og þörfum fyrir gæludýraþjónustu.
+
+Hugmyndin er einföld: samanfletta fólk sem hefur til samfélagslegs áhuga og þörfir til umsýslu, pössunar eða einfaldlega gönguferða með þeim sem hafa gæludýr í umönnun. Með Bellaser getur þú skráð þig sem eigandi eða sem þjónustufulltrúi, miðlað til sín milli og skapað nýjar tengingar.
+
+Það sem skilur Bellaser frá öðrum tækjum er samfelld samskipti milli notenda. Þú getur samskipt við þjónustufulltrúa, skoðað umsagnir og raðað þjónustu eftir þínum þörfum. Það er ekki bara um að fá úrræði fyrir þjónustu, heldur um að byggja samfélag þar sem gæludýraæsku og umönnun er í forgrunni.
+
+Með Bellaser getur þú:
+
+Skráð og stjórnað þínum gæludýraþjónustu þarfir beint frá snjallsímanum þínum.
+Leitað að áreiðanlegum þjónustufulltrúum sem bjóða upp á þjónustu sem passar þínum skilyrðum og þörfum.
+Skoðað umsagnir og einkunnir frá öðrum notendum til að tryggja gæði og traust.
+Búið til samfélag innan appins með deilum, radíóvélar og viðburði.
+Með Bellaser er hundagöngan, kattapössunin og umönnunin við gæludýrið þitt einfaldari og skemmtilegri en nokkurn tíma áður. Byggðu nýja tengsl, styrk þína samkennd og gakktu handan þess sem þú átt að veita og þig nauðsynlegur. Bellaser - fyrir gæludýraæskuna í þér.
+          </p>
+        <h2>
+          Myndbönd:
+        </h2>
+        <a href="https://youtu.be/9gCfWpOVHu4" target='_blank'>- Tutorial á síðunni</a>
+      </div>
+      </div>
+       
+      
+    </>
   );
 };
+
+export default Help;
