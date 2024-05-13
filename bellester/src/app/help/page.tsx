6221c0eb93f85eee6da617e66ff4a1395e03dec3
Kristján Viktor Steinarsson <100709790+kristjansteinarsson@users.noreply.github.com>
2024-05-13 11:59:43 +0000

    Smá breyting

--- bellester/src/app/help/page.tsx
+++ bellester/src/app/help/page.tsx
@@ -6,15 +6,16 @@ const Help = () => {
       <div className='umOkkur'>
         <h1>Hjálp og Aðstoð</h1>
         <div className='umOkkurTexti'>
+          <h2>
+            Github Síða:
+          </h2>
+          <a href="https://github.com/Vefforritun-2-hopur-2/Bellaser" target='_blank'>- Link</a>
         <h2>
           Myndbönd:
         </h2>
         <a href="https://youtu.be/9gCfWpOVHu4" target='_blank'>- Tutorial á síðunni</a>
         <br />
         <a href="/contact"><button>Hafðu Samband</button></a>
-
-        <h3>Eitthvað annað vandamál?</h3>
-        <a href="https://www.dominos.is/" target='_blank'>112</a>
 
       </div>
       </div>
